4180aaf381682c49066e6283172d66ba0ae02185
1455394826@qq.com <1455394826@qq.com>
2018-08-26 20:13:17 +0800

    修改关闭开启debug

--- config/debugbar.php
+++ config/debugbar.php
@@ -14,7 +14,8 @@ return [
      |
      */
 
-    'enabled' => env('DEBUGBAR_ENABLED', null),
+//    'enabled' => env('DEBUGBAR_ENABLED', null),
+    'enabled' => env('APP_DEBUG', null),
     'except' => [
         //
     ],
